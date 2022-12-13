9f333d68ba7506f756a91e95cabc33a11b3d9b69
lamms <sonlam.gun@gmail.com>
2022-12-13 17:16:23 +0700

    'test'

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TextToSpeechService } from 'src/text-to-speech/text-to-speech.service';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [TextToSpeechService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
